63c3935051bcd52711017b87054d27c757230040
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2024-09-12 23:21:43 -0400

    Update setup.lua

--- lua/plugins/lsp/setup.lua
+++ lua/plugins/lsp/setup.lua
@@ -1,5 +1,5 @@
 -- lua/plugins/lsp/setup.lua
-local lspconfig = require('lspconfig')
+local lspconfig = require('nvim-lspconfig')
 local mason_lspconfig = require('mason-lspconfig')
 
 local function setup_server(server_name)
