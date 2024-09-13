2e3a62ac5e572d35e6a161489bd17a2e26b1343e
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2024-09-12 23:38:17 -0400

    Update setup.lua

--- lua/plugins/lsp/setup.lua
+++ lua/plugins/lsp/setup.lua
@@ -6,7 +6,7 @@ local function setup_server(server_name)
     -- Try to load server configuration dynamically
     local success, server_config = pcall(require, 'plugins.lsp.servers.' .. server_name)
     if success and type(server_config) == "table" and type(server_config.setup) == "function" then
-        server_config.setup()  -- Call the setup function without passing an empty table
+        server_config.setup({})  -- Call the setup function without passing an empty table
     else
         print('No configuration found for ' .. server_name)
     end
