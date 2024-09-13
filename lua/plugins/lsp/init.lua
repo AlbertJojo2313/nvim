8c7d1bd127d54af710bc9979333f12c9ebceaabf
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2024-09-12 23:42:29 -0400

    Update init.lua

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,6 @@
 -- Initialize LSP Servers
 return {
-    require('plugins.lsp.lsp-config'),
+    --require('plugins.lsp.lsp-config'),
     require('plugins.lsp.setup'),
     -- Optionally uncomment this if you want to include keymaps
     -- require('plugins.lsp.keymaps')
