c32d40f55b1756d5158821bd282fe9cbae4244d2
AlbertJojo2313 <46793650+AlbertJojo2313@users.noreply.github.com>
2024-09-12 23:06:13 -0400

    Update mason.lua

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,10 +1,14 @@
+-- lua/plugins/lsp/mason.lua
 return {
+    -- Mason.nvim plugin
     {
         'williamboman/mason.nvim',
         config = function()
             require('mason').setup()
         end
     },
+    
+    -- Mason-LSPConfig plugin
     {
         'williamboman/mason-lspconfig.nvim',
         config = function()
@@ -15,20 +19,23 @@ return {
                     "clangd",
                     "ast_grep",
                     "sqlls",
+                    -- Uncomment and add more servers if needed
                     -- "java_language_server",
                     -- "r_language_server",
                     "textlsp",
-                    "tsserver",  -- Updated here
+                    "tsserver",  -- Make sure this is the correct name
                     "ltex",
                 },
             })
         end,
         dependencies = { 'williamboman/mason.nvim' },
     },
+    
+    -- nvim-lspconfig plugin
     {
         'neovim/nvim-lspconfig',
         config = function()
-            -- This will be automatically handled by lazy.nvim based on your lazy setup
+            -- lspconfig setup will be handled by lazy.nvim based on the lazy setup
         end,
         dependencies = { 'williamboman/mason-lspconfig.nvim' },
     },
